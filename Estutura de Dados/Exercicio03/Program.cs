619e48adfc65a97ee3ceea2d68455aac0e1ba5a1
ale-vargas <alexcvjunior@gmail.com>
2023-08-15 08:58:05 -0300

    exercicio03

---
+++ Estutura de Dados/Exercicio03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o 1° numero: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a operação matemática a ser usada (Ex.: + - * /): ");
+            string sinal = Console.ReadLine();
+
+            Console.WriteLine("Digite o 2° numero: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double result;
+
+            if (sinal == "+")
+            {
+                result = num1 + num2;
+                Console.WriteLine($"O resultado da soma é: {result}");
+            }
+            else if (sinal == "-")
+            {
+                result = num1 - num2;
+                Console.WriteLine($"O resultado da subtração é: {result}");
+            }
+            else if (sinal == "*")
+            {
+                result = num1 * num2;
+                Console.WriteLine($"O resultado da multiplicação é {result}");
+            }
+            else
+            {
+                result = num1 / num2;
+                Console.WriteLine($"O resultado da divisão é {result}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
