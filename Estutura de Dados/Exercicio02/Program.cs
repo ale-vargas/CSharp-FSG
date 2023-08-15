419ec9790e2fc473a422bc105fa51b4ba54837bf
ale-vargas <alexcvjunior@gmail.com>
2023-08-15 08:30:05 -0300

    exercicio02

---
+++ Estutura de Dados/Exercicio02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o 1° numero:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2° numero");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"O maior valor informado foi: {num1}");
+            }
+            else
+            {
+                Console.WriteLine($"O maior valor informado foi: {num2}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
