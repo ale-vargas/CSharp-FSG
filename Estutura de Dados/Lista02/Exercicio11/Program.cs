6ac36077a34b2955289ae890d7c6c52f2dad3782
ale-vargas <alexcvjunior@gmail.com>
2023-09-09 10:17:33 -0300

    ex 11-12

---
+++ Estutura de Dados/Lista02/Exercicio11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+11. Faça um programa que exiba os primeiros 10 números naturais usando um loop for.
+*/
+
+namespace Exercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Estutura de Dados/Lista02/Exercicio12/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+12. Escreva um programa que peça ao usuário para adivinhar um número inteiro de 1 a 100. O programa deve fornecer 
+dicas se o número digitado é maior ou menor que o número correto, e contar quantas tentativas foram necessárias.
+*/
+
+namespace Exercicio12
+{
+    public class Program
+    {
+        EntradaDados entradaDados;
+        public Program()
+        {
+            entradaDados = new EntradaDados();
+            Random random = new Random();
+            int certo = random.Next(1, 101);
+            int quant = 0;
+            bool tst = false;
+
+            while (!tst)
+            {
+                int num = entradaDados.LeInteiro("Informe um número: ");
+                quant++;
+
+                if (num < 1 || num > 100)
+                {
+                    Console.WriteLine("Informe números entre 1 e 100.");
+                }
+                else if (num < certo)
+                {
+                    Console.WriteLine("Tente um número maior.");
+                }
+                else if (num > certo)
+                {
+                    Console.WriteLine("Tente um número menor.");
+                }
+                else
+                {
+                    tst = true;
+                }
+            }
+            Console.WriteLine($"Você acertou o número {certo} em {quant} tentativas.");
+        }
+
+        static void Main(string[] args)
+        {
+            Program exec = new Program();
+            Console.ReadKey();
+        }
+    }
+}
