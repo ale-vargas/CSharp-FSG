d6cee0a910fdf1efe4906abe5076ca96ddb53f13
ale-vargas <alexcvjunior@gmail.com>
2023-09-11 14:23:49 -0300

    ex 21-22

---
+++ Estutura de Dados/Lista02/Exercicio21/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+21. Escreva um programa que preencha um vetor com 5 números inteiros informados pelo usuário e em seguida, 
+exiba esses números na ordem inversa.
+*/
+
+namespace Exercicio21
+{
+    public class Program
+    {
+        EntradaDados entradaDados;
+        public Program()
+        {
+            entradaDados = new EntradaDados();
+
+            int[] vetor = new int[5];
+            for (int i = 0; i <= 4; i++)
+            {
+                vetor[i] = entradaDados.LeInteiro($"Posição {i}. Informe um número  inteiro: ");
+            }
+
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine($"Posição {i}: {vetor[i]}.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program exec = new Program();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Estutura de Dados/Lista02/Exercicio22/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+22. Crie um programa que calcule a média dos valores em um vetor de números decimais.
+*/
+namespace Exercicio22
+{
+    public class Program
+    {
+        public Program() 
+        {
+            double[] vetor = {2.4, 3.8, 7.5, 5.2, 6.9};
+
+            double soma = 0;
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                soma += vetor[i]; 
+            }
+
+            double media = soma / vetor.Length; 
+
+            Console.WriteLine($"A média dos valores é: {media}");
+        }
+        static void Main(string[] args)
+        {
+            Program exec = new Program();
+            Console.ReadKey();
+        }
+    }
+}
