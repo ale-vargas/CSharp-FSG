a28187ad8152231153c8244ca937e96051bdc812
ale-vargas <alexcvjunior@gmail.com>
2023-09-16 13:22:07 -0300

    Aluno

---
+++ Estutura de Dados/Lista03/Exercicio06/Aluno.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+/*
+Classes e Atributos:
+• Classe: `Aluno`
+• Atributos: `nome` (string), `matricula` (string), `nota` (double) 
+*/
+
+namespace Exercicio06
+{
+    public class Aluno
+    {
+        private string nome;
+        private string matricula;
+        private double nota;
+
+        public Aluno() 
+        {
+            nome = string.Empty;
+            matricula = string.Empty;
+            nota = 0;
+        }
+
+        public Aluno(string nome, string matricula, double nota)
+        {
+            this.nome = nome;
+            this.matricula = matricula;
+            this.nota = nota;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+        public double Nota
+        {
+            get { return nota; }
+            set { nota = value; }
+        }
+        /*
+        Métodos:
+        • `VerificarAprovacao()`: 
+        Retorna true se o aluno foi aprovado (nota maior ou igual a 6), caso contrário, retorna false
+        */
+
+        public bool VerificarAprovacao()
+        {
+            if (nota >= 6)
+            {
+                Console.WriteLine("Aluno Aprovado!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Aluno Reprovado!");
+                return false;
+            }
+        }
+        public void DadosAluno()
+        {
+            Console.WriteLine($"Nome: {Nome}.");
+            Console.WriteLine($"Matrícula: {Matricula}");
+            Console.WriteLine($"Média: {Nota}");
+        }
+    }
+}
---
+++ Estutura de Dados/Lista03/Exercicio06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Exercício 6: Aluno
+
+Descrição do Problema:
+Crie uma classe chamada `Aluno` com atributos para armazenar o nome, a matrícula e a nota de um aluno. 
+Implemente um método para verificar se o aluno foi aprovado (nota maior ou igual a 6)
+*/
+
+namespace Exercicio06
+{
+    public class Program
+    {
+        Aluno aluno1;
+        public Program()
+        {
+            Aluno aluno1 = new Aluno("João", "12345", 8.0);
+            aluno1.DadosAluno();
+            bool aprovado = aluno1.VerificarAprovacao();
+
+        }
+        static void Main(string[] args)
+        {
+            Program exec = new Program();
+            Console.ReadKey();
+        }
+    }
+}
