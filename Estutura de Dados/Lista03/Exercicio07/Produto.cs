3b5aa3438eb5ad0abd3fde44b9f80c90f7ced778
ale-vargas <alexcvjunior@gmail.com>
2023-09-16 13:47:22 -0300

    Produto

---
+++ Estutura de Dados/Lista03/Exercicio07/Produto.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Classes e Atributos:
+• Classe: `Produto`
+• Atributos: `nome` (string), `preco` (double), `quantidadeEmEstoque` (int)
+*/
+
+namespace Exercicio07
+{
+    public class Produto
+    {
+        private string nome;
+        private double preco;
+        private int quantidadeEmEstoque;
+
+        public Produto()
+        {
+            nome = string.Empty;
+            preco = 0;
+            quantidadeEmEstoque = 0;
+        }
+
+        public Produto(string nome, double preco, int quantidadeEmEstoque)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidadeEmEstoque = quantidadeEmEstoque;
+        }
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public double Preco
+        {
+            get { return preco; }
+            set { preco = value; }
+        }
+        public int QuantidadeEmEstoque
+        {
+            get { return quantidadeEmEstoque; }
+            set { quantidadeEmEstoque = value; }
+        }
+        /*
+         Métodos:
+        • `AdicionarEstoque(int quantidade)`: Aumenta a quantidade em estoque em 
+        `quantidade` unidades.
+        • `RemoverEstoque(int quantidade)`: Diminui a quantidade em estoque em `quantidade` unidades
+         */
+
+        public void AdicionarEstoque(int valor)
+        {
+            quantidadeEmEstoque += valor;
+            Console.WriteLine($"Quantidade em estoque após adicionar: {QuantidadeEmEstoque}");
+        }
+
+        public void RemoverEstoque(int valor)
+        {
+            quantidadeEmEstoque -= valor;
+            Console.WriteLine($"Quantidade em estoque após remover: {QuantidadeEmEstoque}");
+        }
+        public void DadosProduto()
+        {
+            Console.WriteLine($"Produto: {Nome}");
+            Console.WriteLine($"Preço: {Preco}");
+            Console.WriteLine($"Quantidade inicial em estoque: {QuantidadeEmEstoque}");
+        }
+    }
+}
---
+++ Estutura de Dados/Lista03/Exercicio07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Exercício 7: Produto
+Descrição do Problema:
+Crie uma classe chamada `Produto` com atributos para armazenar o nome, o preço e a quantidade em estoque de um produto. 
+Implemente métodos para aumentar e diminuir a quantidade em estoque.
+*/
+
+namespace Exercicio07
+{
+    public class Program
+    {
+        Produto produto1;
+        public Program() 
+        {
+            Produto produto1 = new Produto("Camiseta", 20.0, 50);
+            produto1.DadosProduto();
+            produto1.AdicionarEstoque(10);
+            produto1.RemoverEstoque(5);
+        }
+        static void Main(string[] args)
+        {
+            Program exec = new Program();
+            Console.ReadKey();
+        }
+    }
+}
