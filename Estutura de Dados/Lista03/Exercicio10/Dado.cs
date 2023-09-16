60460f496a7b85e613386d23c53bf594dd34c57e
ale-vargas <alexcvjunior@gmail.com>
2023-09-16 14:56:35 -0300

    Dado

---
+++ Estutura de Dados/Lista03/Exercicio10/Dado.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Classes e Atributos:
+• Classe: `Dado`
+• Atributo: Nenhum
+*/
+
+namespace Exercicio10
+{
+    public class Dado
+    {
+        private Random random;
+
+        public Dado()
+        {
+            random = new Random();
+        }
+
+        /*
+        Métodos:
+        • `RolarDado()`: Retorna um valor aleatório de 1 a 6.
+        */
+
+        public int RolarDado()
+        {
+            int resultado = random.Next(1, 7);
+            return resultado;
+        }
+    }
+}
---
+++ Estutura de Dados/Lista03/Exercicio10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Exercício 10: Jogo de Dados
+
+Descrição do Problema:
+Crie uma classe chamada `Dado` que representa um dado de seis faces. 
+Implemente um método para rolar o dado e obter um valor aleatório de 1 a 6
+*/
+
+namespace Exercicio10
+{
+    public class Program
+    {
+        Dado meuDado;
+        public Program() 
+        {
+            meuDado = new Dado();
+            int resultado = meuDado.RolarDado();
+            Console.WriteLine(resultado);
+        }
+        static void Main(string[] args)
+        {
+            Program exec = new Program();
+            Console.ReadKey();
+        }
+    }
+}
