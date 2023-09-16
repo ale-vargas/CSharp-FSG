c35a2a32f13f2e51dc39f24bd45fab91fdf5aad2
ale-vargas <alexcvjunior@gmail.com>
2023-09-16 12:02:42 -0300

    Pessoa

---
+++ Estutura de Dados/Lista03/Exercicio04/Pessoa.cs
@@ -0,0 +1,65 @@
+﻿using Exercicio04;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+/*
+Classes e Atributos:
+• Classe: `Pessoa`
+• Atributos: `nome` (string), `idade` (int), `endereco` (string)
+*/
+
+namespace Exercicio04
+{
+    public class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private string endereco;
+
+        public Pessoa()
+        {
+            nome = string.Empty;
+            idade = 0;
+            endereco = string.Empty;
+        }
+
+        public Pessoa(string nome, int idade, string endereco)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.endereco = endereco;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public int Idade
+        {
+            get { return idade; }
+            set { idade = value; }
+        }
+        public string Endereco
+        {
+            get { return endereco; }
+            set { endereco = value; }
+        }
+
+        /*
+        Métodos:
+        • `MostrarDetalhes()`: Exibe os detalhes da pessoa.
+        */
+
+        public void MostrarDetalhes()
+        {
+            Console.WriteLine($"Nome: {nome}.");
+            Console.WriteLine($"Idade: {idade} anos.");
+            Console.WriteLine($"Endereço: {endereco}.");
+        }
+    }
+}
---
+++ Estutura de Dados/Lista03/Exercicio04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Exercício 4: Pessoa
+
+Descrição do Problema:
+Crie uma classe chamada `Pessoa` com atributos para armazenar o nome, a idade e o endereço de uma pessoa. 
+Implemente um método para exibir os detalhes da pessoa.
+*/
+
+namespace Exercicio04
+{
+    
+    public class Program
+    {   
+        Pessoa pessoal;
+        public Program() 
+        {
+            Pessoa pessoa1 = new Pessoa("Maria", 30, "Rua ABC, 123");
+            pessoa1.MostrarDetalhes();
+
+        }
+        static void Main(string[] args)
+        {
+            Program exec = new Program();
+            Console.ReadKey();
+        }
+    }
+}
