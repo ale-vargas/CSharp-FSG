8e9a323506137c15b8e445d58abfc674ce988c28
ale-vargas <alexcvjunior@gmail.com>
2023-09-16 11:49:32 -0300

    Carro

---
+++ Estutura de Dados/Lista03/Exercicio03/Carro.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Classes e Atributos:
+• Classe: `Carro`
+• Atributos: `marca` (string), `modelo` (string), `velocidadeAtual` (int)
+*/
+
+namespace Exercicio03
+{
+    public class Carro
+    {
+        private string marca;
+        private string modelo;
+        private int velocidadeAtual;
+
+        public Carro()
+        {
+            marca = string.Empty;
+            modelo = string.Empty;
+            velocidadeAtual = 0;
+        }
+
+        public Carro(string marca, string modelo, int velocidadeAtual)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.velocidadeAtual = velocidadeAtual;
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        } 
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+        public int VelocidadeAtual
+        {
+            get { return velocidadeAtual; }
+            set { velocidadeAtual = value; }
+        }
+
+        /*    
+        Métodos:
+        • `Acelerar(int aumento)`: Aumenta a velocidade atual do carro em `aumento` km/h.
+        • `Desacelerar(int reducao)`: Reduz a velocidade atual do carro em `reducao` km/h.
+        */
+
+        public void Acelerar(int valor)
+        {
+            velocidadeAtual += valor;
+            Console.WriteLine($"Velodidade após acelerar é de {velocidadeAtual} Km/h.");
+        }
+
+        public void Desacelerar(int valor)
+        {
+            velocidadeAtual -= valor;
+            Console.WriteLine($"Velodidade após desacelerar é de {velocidadeAtual} Km/h.");
+        }
+        public void ExibirDadosCarro()
+        {
+            Console.WriteLine($"Marca: {Marca}");
+            Console.WriteLine($"Modelo: {Modelo}");
+            Console.WriteLine($"Velocidade Inicial: {velocidadeAtual}");
+        }
+    }
+}
---
+++ Estutura de Dados/Lista03/Exercicio03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Exercício 3: Carro
+
+Descrição do Problema:
+Crie uma classe chamada `Carro` com atributos para armazenar a marca, o modelo e a velocidade atual do carro. 
+Implemente métodos para acelerar e desacelerar o carro.
+*/
+
+namespace Exercicio03
+{
+    public class Program
+    {
+        Carro meuCarro;
+        public Program() 
+        {
+            Carro meuCarro = new Carro("Ford", "Focus", 60);
+            meuCarro.ExibirDadosCarro();
+            meuCarro.Acelerar(20);
+            meuCarro.Desacelerar(10);
+
+        }
+        static void Main(string[] args)
+        {
+            Program exec = new Program();
+            Console.ReadKey();
+        }
+    }
+}
