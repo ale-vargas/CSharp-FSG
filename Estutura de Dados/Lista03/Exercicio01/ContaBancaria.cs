1f2b8df5c8f05961d81e8c0deee00b242ad1eddc
ale-vargas <alexcvjunior@gmail.com>
2023-09-16 08:01:52 -0300

    ContaBancaria

---
+++ Estutura de Dados/Lista03/Exercicio01/ContaBancaria.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+- Classe: `ContaBancaria`
+- Atributos: `numeroConta` (int), `nomeTitular` (string), `saldo` (double)
+*/
+
+namespace Exercicio01
+{
+    public class ContaBancaria
+    {
+        public int numeroConta { get; private set; }
+        public string nomeTitular { get; private set; }
+        public double saldo { get; private set; }
+
+
+        public ContaBancaria(int numeroConta, string nomeTitular, double saldo)
+        {
+            this.numeroConta = numeroConta;
+            this.nomeTitular = nomeTitular;
+            this.saldo = saldo;
+        }
+
+        /*
+         Métodos:
+        - `Depositar(double valor)`: Adiciona o valor especificado ao saldo.
+        - `Sacar(double valor)`: Retira o valor especificado do saldo, desde que haja saldo suficiente.
+        */
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                saldo += valor;
+                Console.WriteLine($"Realizado depósito no valor de: R$ {valor}.");
+                Console.WriteLine($"Saldo na conta é de: R${saldo}.");
+            }
+            else
+            {
+                Console.WriteLine("ERRO! Nenhum valor informado para depósito.");
+            }
+        }
+        public void Sacar(double valor)
+        {
+            if (valor > 0)
+            {
+                if (saldo >= valor)
+                {
+                    saldo -= valor;
+                    Console.WriteLine($"Realizado saque no valor de: R$ {valor}.");
+                    Console.WriteLine($"Saldo na conta é de: R${saldo}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Saque NÃO aprovado. Saldo insuficiente!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERRO! Nenhum valor informado para saque.");
+            }
+        }
+    }
+}
---
+++ Estutura de Dados/Lista03/Exercicio01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Exercício 1: Conta Bancária
+
+Descrição do Problema:
+Crie uma classe chamada `ContaBancaria` com atributos para armazenar o número da conta, o nome do titular e o saldo. 
+Implemente métodos para depositar e sacar dinheiro da conta.
+*/
+
+namespace Exercicio01
+{
+    public class Program
+    {
+        ContaBancaria minhaConta;
+        public Program()
+        {
+            minhaConta = new ContaBancaria(12345, "João", 1000.0);
+
+            Console.WriteLine($"Número da Conta: {minhaConta.numeroConta}");
+            Console.WriteLine($"Nome do Titular: {minhaConta.nomeTitular}");
+            Console.WriteLine($"Saldo da Conta: R$ {minhaConta.saldo}");
+
+            minhaConta.Depositar(500.0);
+            minhaConta.Sacar(200.0);
+        }
+        static void Main(string[] args)
+        {
+            Program execc = new Program();
+            Console.ReadKey();
+        }
+    }
+}
