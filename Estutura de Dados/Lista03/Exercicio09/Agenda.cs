891552a2dd69e490249ec2c43912b4f82fd771c0
ale-vargas <alexcvjunior@gmail.com>
2023-09-16 14:45:32 -0300

    Agenda

---
+++ Estutura de Dados/Lista03/Exercicio09/Agenda.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Classes e Atributos:
+• Classe: `Agenda`
+• Atributos: `contatos` (array de strings para nomes), `telefones` (array de strings para números de telefone)
+*/
+
+namespace Exercicio09
+{
+    public class Agenda
+    {
+        private string[] contatos;
+        private string[] telefones;
+        private int espacoAgenda;
+
+        public Agenda()
+        {
+            contatos = new string[5];
+            telefones = new string[5];
+            espacoAgenda = 0;
+        }
+
+        /*
+        Métodos:
+        • - `AdicionarContato(string nome, string telefone)`: Adiciona um contato à agenda.
+        • - `ListarContatos()`: Lista os contatos da agenda.
+        */
+
+        public void AdicionarContato(string nome, string telefone)
+        {
+            if (espacoAgenda < 5)
+            {
+                contatos[espacoAgenda] = nome;
+                telefones[espacoAgenda] = telefone;
+                espacoAgenda++;
+            }
+            else
+            {
+                Console.WriteLine("A agenda está cheia.");
+            }
+        }
+        public void ListarContatos()
+        {
+            Console.WriteLine("-- Agenda --");
+            for (int i = 0; i < espacoAgenda; i++)
+            {
+                Console.WriteLine($"Nome: {contatos[i]}, telefone: {telefones[i]}");
+            }
+        }
+    }
+}
---
+++ Estutura de Dados/Lista03/Exercicio09/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Exercício 9: Agenda
+
+Descrição do Problema:
+Crie uma classe chamada `Agenda` que pode armazenar até 5 contatos. Cada contato deve conter um nome e um número de telefone. 
+Implemente métodos para adicionar e listar contatos.
+*/
+
+namespace Exercicio09
+{
+    public class Program
+    {
+        Agenda minhaAgenda;
+        public Program() 
+        {
+            Agenda minhaAgenda = new Agenda();
+            minhaAgenda.AdicionarContato("Maria", "123-456-7890");
+            minhaAgenda.AdicionarContato("João", "987-654-3210");
+            minhaAgenda.ListarContatos();
+        }
+        static void Main(string[] args)
+        {
+            Program exe = new Program();
+            Console.ReadKey();
+        }
+    }
+}
