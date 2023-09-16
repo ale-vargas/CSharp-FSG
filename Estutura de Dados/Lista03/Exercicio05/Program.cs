ed418650fe640524f5c8af3caa1b918a517485be
ale-vargas <alexcvjunior@gmail.com>
2023-09-16 12:55:33 -0300

    Retangulo

---
+++ Estutura de Dados/Lista03/Exercicio05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Exercício 5: Retângulo
+
+Descrição do Problema:
+Crie uma classe chamada `Retangulo` com atributos para armazenar a largura e a altura de um retângulo. 
+Implemente um método para calcular a área do retângulo.
+*/
+
+namespace Exercicio05
+{
+    public class Program
+    {
+        Retangulo meuRetangulo;
+        public Program()
+        {
+            meuRetangulo = new Retangulo(5.0, 3.0);
+            double area = meuRetangulo.CalcularArea();
+        }
+        static void Main(string[] args)
+        {
+            Program exec = new Program();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Estutura de Dados/Lista03/Exercicio05/Retangulo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Classes e Atributos:
+• Classe: `Retangulo`
+• Atributos: `largura` (double), `altura` (double)
+*/
+
+namespace Exercicio05
+{
+    public class Retangulo
+    {
+        private double largura;
+        private double altura;
+
+        public Retangulo()
+        {
+            largura = 0;
+            altura = 0;
+        }
+
+        public Retangulo(double largura, double altura) 
+        { 
+            this.largura = largura;
+            this.altura = altura;
+        }
+
+        public double Largura
+        {
+            get { return largura; }
+            set { largura = value; }
+        } 
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+        /*
+        Métodos:
+        • `CalcularArea()`: Retorna a área do retângulo(largura* altura).
+        */
+
+        public double CalcularArea()
+        {
+            double area = largura*altura;
+            Console.WriteLine($"A área é: {area} m².");
+
+            return area;
+        }
+
+    }
+}
