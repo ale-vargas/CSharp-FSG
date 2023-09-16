562ca3e5588dcc6dc2b5672de3455568adbef14b
ale-vargas <alexcvjunior@gmail.com>
2023-09-16 10:20:56 -0300

    Livro

---
+++ Estutura de Dados/Lista03/Exercicio02/Livro.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Classes e Atributos:
+• Classe: `Livro`
+• Atributos: `titulo` (string), `autor` (string), `numeroPaginas` (int
+*/
+
+namespace Exercicio02
+{
+    public class Livro
+    {
+        private string titulo;
+        private string autor;
+        private int numeroPaginas;
+
+        public Livro()
+        {
+            titulo = string.Empty;
+            autor = string.Empty;
+            numeroPaginas = 0;
+        }
+
+        public Livro(string titulo, string autor, int numeroPaginas)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.numeroPaginas = numeroPaginas;
+        }
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+        public string Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+        public int NumeroPaginas
+        {
+            get { return numeroPaginas; }
+            set { numeroPaginas = value; }
+        }
+
+        /*
+        Métodos:
+        • `MostrarInformacoes()`: Exibe as informações do livro
+        */
+
+        public void MostrarInformacoes()
+        {
+            Console.WriteLine("Informações do livro: ");
+            Console.WriteLine($"Titulo: {Titulo}.");
+            Console.WriteLine($"Autor: {Autor}.");
+            Console.WriteLine($"Número de Páginas: {NumeroPaginas}.");
+        }
+    }
+}
---
+++ Estutura de Dados/Lista03/Exercicio02/Program.cs
@@ -0,0 +1,32 @@
+﻿using Exercicio02;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Exercício 2: Livro
+
+Descrição do Problema:
+Crie uma classe chamada `Livro` com atributos para armazenar o título, o autor e o número de páginas. 
+Implemente um método para exibir informações sobre o livro.
+*/
+
+namespace Exercicio02
+{
+    public class Program
+    {
+        Livro livro;
+        public Program() 
+        { 
+            livro = new Livro("Aventuras de Alice", "Lewis Carroll", 200);
+            livro.MostrarInformacoes();
+        }
+        static void Main(string[] args)
+        {
+            Program exec = new Program();
+            Console.ReadKey();
+        }
+    }
+}
