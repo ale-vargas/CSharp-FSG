4588e7d4cfe39571591d2117fdf8d5d4b6ba2d7c
ale-vargas <alexcvjunior@gmail.com>
2023-10-16 13:40:07 -0300

    Lista Duplamente Encadeada

---
+++ Estutura de Dados/Lista04/ListaDuplamenteEncadeada/ListaDuplamenteEncadeada.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDuplamenteEncadeada
+{
+    public class ListaDuplamenteEncadeada
+    {
+        private Nodo _raiz;
+
+        /* Exercício 1: Lista Duplamente Encadeada 
+        Descrição do Problema: Crie uma lista encadeada simples que permita a inserção e remoção de elementos. */
+
+        // Inserir(valor): Insere um elemento no final da lista.
+
+        public void Inserir(int valor)
+        {
+            Nodo novoNodo = new Nodo(valor);
+
+            if (_raiz == null)
+            {
+                _raiz = novoNodo;
+            }
+            else
+            {
+                Nodo nodo = _raiz;
+                while (nodo.Proximo != null)
+                {
+                    nodo = nodo.Proximo;
+                }
+
+                novoNodo.Anterior = nodo;
+                nodo.Proximo = novoNodo;
+            }
+        }
+
+        // Exibir(): Exibe os elementos da lista.
+        public void Exibir()
+        {
+            if (_raiz == null)
+            {
+                Console.WriteLine("A lista está vazia.");
+                return;
+            }
+
+            Nodo nodo = _raiz;
+
+            while (nodo != null)
+            {
+                Console.Write($"{nodo.Conteudo}");
+
+                if (nodo.Proximo != null)
+                {
+                    Console.Write($" -> ");
+                }
+
+                nodo = nodo.Proximo;
+            }
+
+            Console.WriteLine("");
+        }
+
+        //Remover(valor): Remove a primeira ocorrência do elemento com o valor especificado. 
+        public void Remover(int valor)
+        {
+            if (_raiz == null)
+            {
+                Console.WriteLine("A lista está vazia.");
+                return;
+            }
+
+            if (_raiz.Conteudo == valor)
+            {
+                _raiz = _raiz.Proximo;
+                if (_raiz != null)
+                {
+                    _raiz.Anterior = null;
+                }
+                return;
+            }
+
+            Nodo nodoAtual = _raiz;
+
+            while (nodoAtual != null && nodoAtual.Conteudo != valor)
+            {
+                nodoAtual = nodoAtual.Proximo;
+            }
+
+            if (nodoAtual == null)
+            {
+                Console.WriteLine($"Valor não encontrado na lista.");
+                return;
+            }
+
+            if (nodoAtual.Anterior != null)
+            {
+                nodoAtual.Anterior.Proximo = nodoAtual.Proximo;
+            }
+
+            if (nodoAtual.Proximo != null)
+            {
+                nodoAtual.Proximo.Anterior = nodoAtual.Anterior;
+            }
+        }
+
+    }
+}
---
+++ Estutura de Dados/Lista04/ListaDuplamenteEncadeada/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDuplamenteEncadeada
+{
+    public class Program
+    {
+        public Program() 
+        {
+            ListaDuplamenteEncadeada lista = new ListaDuplamenteEncadeada();
+
+            // Exercício 1: Lista Encadeada Simples Básica
+       
+            lista.Inserir(10);
+            lista.Inserir(20);
+            lista.Inserir(30);
+            lista.Exibir(); // Saída: 10 -> 20 -> 30
+            lista.Remover(20);
+            lista.Exibir(); // Saída: 10 -> 30
+            
+        }
+        static void Main(string[] args)
+        {
+            Program exec = new Program();
+            Console.ReadKey();
+        }
+    }
+}
